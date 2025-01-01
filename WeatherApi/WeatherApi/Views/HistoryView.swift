a19540f1ab7cd1955d440380e2677e729a3799e5
Oren Leavitt <oleavitt@ix.netcom.com>
2025-01-01 15:54:38 -0600

    Add "Delete all History" option in Settings

--- WeatherApi/WeatherApi/Views/HistoryView.swift
+++ WeatherApi/WeatherApi/Views/HistoryView.swift
@@ -65,31 +65,23 @@ private extension HistoryView {
 }
 
 #Preview {
-    let mockHistory = [
-        CurrentWeatherModel(location: "Dallas, Texas",
-                            epochUpdated: 1000,
-                            dateTime: Date.now,
-                            tempC: 15.5, tempF: 65.4,
-                            icon: "//cdn.weatherapi.com/weather/64x64/day/113.png",
-                            code: 1000,
-                            uv: 3, isDay: true),
-        CurrentWeatherModel(location: "Dallas, Texas",
-                            epochUpdated: 1001,
-                            dateTime: Date.now + 900,
-                            tempC: 15.8, tempF: 66.1,
-                            icon: "//cdn.weatherapi.com/weather/64x64/day/113.png",
-                            code: 1000,
-                            uv: 3, isDay: true),
-        CurrentWeatherModel(location: "Dallas, Texas",
-                            epochUpdated: 1002,
-                            dateTime: Date.now + 1800,
-                            tempC: 16.7, tempF: 68,
-                            icon: "//cdn.weatherapi.com/weather/64x64/day/113.png",
-                            code: 1000,
-                            uv: 3, isDay: true)
-    ]
     NavigationStack {
         HistoryView()
-            .modelContainer(for: CurrentWeatherModel.self)
+            .modelContainer(for: CurrentWeatherModel.self) { result in
+                switch result {
+                case .success(let container):
+                    // Load up the model with some mock content
+                    let context = container.mainContext
+                    context.insert(CurrentWeatherModel(location: "Dallas, Texas",
+                                                       epochUpdated: 1000,
+                                                       dateTime: Date.now,
+                                                       tempC: 15.5, tempF: 65.4,
+                                                       icon: "//cdn.weatherapi.com/weather/64x64/day/113.png",
+                                                       code: 1000,
+                                                       uv: 3, isDay: true))
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
     }
 }
--- WeatherApi/WeatherApi/Views/SettingsView.swift
+++ WeatherApi/WeatherApi/Views/SettingsView.swift
@@ -8,13 +8,46 @@
 import SwiftUI
 
 struct SettingsView: View {
+    
+    @Environment(\.modelContext) var context
+    @State var showDeleteHistoryConfirm = false
+    
     var body: some View {
         List {
-            NavigationLink(destination: ApiProviderView()) {
-                Text("api-providers")
+            Section("data-source") {
+                NavigationLink(destination: ApiProviderView()) {
+                    Text("api-providers")
+                }
+            }
+            Section("history") {
+                Button("delete-all-history") {
+                    showDeleteHistoryConfirm.toggle()
+                }
             }
         }
         .navigationTitle("settings")
+        .confirmationDialog("delete-all-history-confirm",
+                            isPresented: $showDeleteHistoryConfirm,
+                            titleVisibility: .visible) {
+            Button(role: .destructive) {
+                deleteAllHistory()
+            } label: {
+                Label("delete", systemImage: "trash")
+                    .symbolVariant(.fill)
+            }
+        }
+    }
+}
+
+extension SettingsView {
+    func deleteAllHistory() {
+        do {
+            try context.delete(model: CurrentWeatherModel.self)
+        } catch {
+#if DEBUG
+            print("Error trying to delete all History. \(error.localizedDescription)")
+#endif
+        }
     }
 }
 
