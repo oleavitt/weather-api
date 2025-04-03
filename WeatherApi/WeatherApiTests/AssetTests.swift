8d742657def952b88b31438eedcfcdec069d8be5
Oren Leavitt <oleavitt@ix.netcom.com>
2025-04-03 15:22:24 -0500

    Move color assets to separate Colors.xcassets file Add AssetTests unit tests to verify valid colors exist.

---
+++ WeatherApi/WeatherApiTests/AssetTests.swift
@@ -0,0 +1,18 @@
+//
+//  AssetTests.swift
+//  WeatherApiTests
+//
+//  Created by Oren Leavitt on 4/3/25.
+//
+
+import XCTest
+
+final class AssetTests: XCTestCase {
+    func testColorsExist() {
+        let allColors = ["cellBackground", "high", "low"]
+
+        for color in allColors {
+            XCTAssertNotNil(UIColor(named: color), "Failed to load color '\(color)' from asset catalog.")
+        }
+    }
+}
